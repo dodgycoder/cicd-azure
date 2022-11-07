0ea79e789d7846a3915ce72ae3645dc9a273a838
Arnab Roy <60926235+arniecommits@users.noreply.github.com>
2022-11-07 20:08:02 +0000

    Updated roles

--- variables.tf
+++ variables.tf
@@ -15,14 +15,14 @@ variable "tags" {
 variable "storage" {
   type = map(string)
   default = {
-    "account" = "zslabstorageattackdemo"
-    "blobname" = "zslabwebappblobpdf123"
+    "account" = "pdflabstorageattackdemo"
+    "blobname" = "pdflabwebappblobpdf123"
   }
 }
 
 
 variable "resource_group_name_prefix" {
-  default     = "ZS-ARNAB-LAB-Attack-Demo"
+  default     = "ARNAB-LAB-PDF-APP-DEMO"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
